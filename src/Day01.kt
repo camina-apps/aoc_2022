12a4977ac1efe1118b2ea48fec715963354500a6
Carmen Geelhaar <carmen.geelhaar@maibornwolff.de>
2022-12-01 13:33:19 +0100

    day 1 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
+    fun caloriesPerElve(rucksack: String) =
+        rucksack.replace(",", "")
+            .trim()
+            .split(" ")
+            .sumOf { it.toInt() }
+
+
+    fun getElvesList(input: List<String>): List<String> =
+        input.toString()
+            .removePrefix("[")
+            .removeSuffix("]")
+            .split(" ,")
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = getElvesList(input)
+        return elves.map { caloriesPerElve(it) }.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = getElvesList(input)
+        return  elves
+            .map { caloriesPerElve(it) }
+            .sortedDescending()
+            .slice(0..2)
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
